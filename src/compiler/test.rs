173ce4cd1c7e311fff0a95cd2b931b53a7b42c6d
Leonardone <neetsdkasu@gmail.com>
2021-04-05 07:46:25 +0900

    integer sort sample

--- src/compiler/test.rs
+++ src/compiler/test.rs
@@ -1225,3 +1225,51 @@ Print s
 
     assert!(!statements.is_empty()); // dummy assert
 }
+
+#[test]
+fn integer_sorting_works() {
+    let src = r#"
+' *** INTSORT ***
+Dim c As Integer
+Dim i As Integer
+Dim j As Integer
+Dim p As Integer
+Dim s As String
+Dim t As String
+Dim arr(255) As Integer
+Print "NUMBERS?"
+Input s
+arr(0) = CInt(s)
+For i = 0 To Len(s) - 1
+    If s(i) = ","c Then
+        c += 1
+        arr(c) = CInt(Mid(s, i + 1))
+    End If
+Next i
+For i = 0 To c
+    For j = 0 To c - 1 - i
+        p = arr(j + 1)
+        If arr(j) < p Then
+            arr(j + 1) = arr(j)
+            arr(j) = p
+        End If
+    Next j
+    t = t & CStr(arr(j)) & ","
+Next i
+Print t
+"#;
+
+    let mut cursor = std::io::Cursor::new(src);
+
+    let code = parser::parse(&mut cursor).unwrap().unwrap();
+
+    let statements = compile("INTSORT", &code[..]).unwrap();
+
+    statements.iter().for_each(|line| {
+        eprintln!("{}", line);
+    });
+
+    eprintln!("{}", crate::stat::analyze(&statements));
+
+    assert!(!statements.is_empty()); // dummy assert
+}
